c3c85a27efd953f1fadb6119e21eed7ea0234f47
natethedequate <white.n@northeastern.edu>
2022-05-01 19:23:19 -0400

    Save:

--- pi.cu
+++ pi.cu
@@ -1,11 +1,40 @@
 #include <cstdio>
 #include <math.h>
 
+#define A(n) (4*n+1)
+#define B(n) (4*n+3)
+#define C(n) (10*n+1)
+#define D(n) (10*n+3)
+#define E(n) (10*n+5)
+#define F(n) (10*n+7)
+#define G(n) (10*n+9)
+
+struct fraction {
+  unsigned int num;
+  unsigned int den;
+};
+
+// adds/subtracts atomically the quotient shifted right by bit_offset bits,  as an int, at the given index of res
+unsigned int dividerAddSub(unsigned int numerator, unsigned int denominator, unsigned int* res, unsigned int bit_offset, unsigned int index, bool adding){
+  unsigned int operand =  ((numerator*sizeof(unsigned int)) >> bit_offset) / denominator;
+  if(adding){
+  while( (long unsigned int)  (atomicAdd_system(res[index], operand) + operand) > (unsigned int) 1 >> (2,4*sizeof(unsigned int))){
+    operand=1;
+    index--;
+  }
+  else while( (double)  (atomicSub_system(res[index], operand) - operand) >  (unsigned int) 1 >>  (2,4*sizeof(unsigned int))){
+    operand=1;
+    index++;
+  }
+  return ((numerator*sizeof(unsigned int)) >> bit_offset) % denominator;
+ 
+} 
+/*
 struct mixednumber{
    int whole;
    int numerator;
    int denominator;
-};:ww
+
 
 __global__ mixednumber divideBase2(int numerator, int denominator){
     struct mixednumber ret = { 
@@ -49,5 +78,17 @@ int main(int _, char **argv){
   cudaDeviceSynchronize();
   printf("%f \n", result*pow(2.0,-6.0));
   return 0;
+}
+*/
+
+int main(){
+  int res[50];
+  assert(dividerAddSub((unsigned int) 1,(unsigned int) 2, res,(unsigned int)0,(unsigned int)2, true)==0);
+  assert(res[2] == (unsigned int) 1 << (sizeof(unsigned int)*4-1));
+  assert(dividerAddSub((unsigned int) 1,(unsigned int) 3, res,(unsigned int)2,(unsigned int)3, true)==1);
+  assert(res[3] == (unsigned int)1 << (sizeof(unsigned int)*4-1));
+  return 0;
+}
+
 }
  
